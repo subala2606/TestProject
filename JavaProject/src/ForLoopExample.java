37eef67e7c8040483c936253410478f9db847217
subala2606 <subala2606@gmail.com>
2019-02-18 23:52:07 -0500

    added forloop class

---
+++ JavaProject/src/ForLoopExample.java
@@ -0,0 +1,29 @@
+
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+	
+        for(int i=0;i<5;i++) {                         
+			System.out.print(i);
+			
+			for(int j=i;j<5;j++) {
+				System.out.print(i);
+				
+			System.out.print(" ");}
+			
+				for(int k=0;k<=i;k++) {
+				System.out.print(" *");
+							
+				
+			}
+			System.out.println();	
+				
+			
+		}
+		
+		
+	}
+}
+
+        	
